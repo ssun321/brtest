73bb96bd39ff2561548101278abc39d94484cefa
tim <tmozii1@gmail.com>
2013-03-14 20:03:13 +0900

    설명화면 추가

---
+++ BrainHealth/BrainHealth/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  BrainHealth
+//
+//  Created by Tim on 13. 3. 14..
+//  Copyright (c) 2013년 Tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
