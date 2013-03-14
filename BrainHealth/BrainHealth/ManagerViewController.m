bf94753951ecc2ce35c25ec13a89860b77f228e0
timzero <timzero@timzeroui-iPhone.local>
2013-03-14 18:23:47 +0900

    Manager 추가

---
+++ BrainHealth/BrainHealth/ManagerViewController.m
@@ -0,0 +1,38 @@
+//
+//  ManagerViewController.m
+//  BrainHealth
+//
+//  Created by timzero on 13. 3. 14..
+//  Copyright (c) 2013년 Tim. All rights reserved.
+//
+
+#import "ManagerViewController.h"
+
+@interface ManagerViewController ()
+
+@end
+
+@implementation ManagerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
